68676d15fdc048b1c419c8770f7f3abdb498d0d6
Bizzy211 <Bizzy211@gmail.com>
2021-04-06 23:17:52 -0400

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -6,12 +6,12 @@ ServerSocket = socket.socket()
 
 
 # Get the assigned container IP Address
-hostName = socket.gethostname()
-ipAddress = socket.gethostbyname(hostName)
-print('Container IP: ' + ipAddress)
+#hostName = socket.gethostname()
+#ipAddress = socket.gethostbyname(hostName)
+#print('Container IP: ' + ipAddress)
 
-host = input("What is the Server IP Address or Hostname? (default: " + ipAddress + ") ") or ipAddress
-port = int(input("What is the Server Port? (default: 1223) ") or "1223")
+host = input('What is the Server IP Address or Hostname?')
+port = int(input('What is the Server Port?')
 
 ThreadCount = 0
 try:
